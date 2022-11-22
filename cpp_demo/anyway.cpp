d8ce3d8b9c0e2ccc77bf162a4909bfad29baaaa8
GW00280825 <837085581@qq.com>
2022-11-21 18:29:28 -0800

    练习

---
+++ cpp_demo/anyway.cpp
@@ -0,0 +1,9 @@
+/**
+ * @file anyway.cpp
+ * @author 许营博
+ * @brief  练习文件，什么都写
+ * @version 0.1
+ * @date 2022-11-21
+ * 
+ */
+
--- cpp_demo/per.cpp
+++ cpp_demo/per.cpp
@@ -31,31 +31,76 @@
 //     return 0;
 // }
 
+// #include <iostream>
+// #include <string>
+// #include "cstring"
+// #include <map>
+// using namespace std;
+
+// struct person {
+// 	std::string name;
+// 	int age;
+// 	std::map<char,int> src;
+// };
+
+// int main() {
+
+// 	person one{"abc", 40};
+// 	one.src['a'] = 20;
+// 	string str{""};
+
+//     memcpy(&str,&one,sizeof(one));
+
+// 	cout << "字符串:"  << str << endl;
+
+// 	person three{};
+
+// 	memcpy(&three,&str, sizeof(one));
+
+// 	cout << three.name << "   " << three.age << endl;
+
+// 	return 0;
+// }
+
 #include <iostream>
 #include <string>
-#include "cstring"
+#include <map>
+#include <cstring>
 using namespace std;
 
-struct person {
-	std::string name;
-	int age;
+#pragma pack(push,1)
+struct str{
+	map<char, int> mp;
 };
+#pragma pack(pop)
 
 int main() {
+	str str1;
+	str1.mp['a'] = 222;
+	str1.mp['b'] = 333;
+	str1.mp['c'] = 444;
+	str1.mp['d'] = 555;
+	str1.mp['e'] = 666;
+	str1.mp['f'] = 777;
+	// for (map<char, int>::iterator it = mp.begin(); it != mp.end(); it++) {
+	// 	cout << it->first;
+	// 	cout << " ";
+	// 	cout << it->second;
+	// 	cout << endl;
+	// }
 
-	person one{"abc", 40};
+	char des[sizeof(str1)]={};
+	// memcpy(des, &str1, sizeof(str1));
 
-	string str{""};
-
-    memcpy(&str,&one,sizeof(struct person));
-
-	cout << "字符串:"  << str << endl;
-
-	person three{};
-
-	memcpy(&three,&str, sizeof(struct person));
-
-	cout << three.name << "   " << three.age << endl;
-
+	str str2;
+	memcpy(&str2, &str1, sizeof(str1));
+	// memcpy(&str2, des, sizeof(str2));
+	// for (map<char, int>::iterator it = mp2.begin(); it != mp2.end(); it++) {
+	// 	cout << it->first;
+	// 	cout << " ";
+	// 	cout << it->second;
+	// 	cout << endl;
+	// }
+	cout << "str2:" << sizeof(str2) << endl;
 	return 0;
 }
