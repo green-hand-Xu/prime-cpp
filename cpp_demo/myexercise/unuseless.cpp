10849c668a2554209a9ce5d60506693086e2eda2
许营博 <gw00280825@tech.gwm.cn>
2023-03-28 18:02:41 +0800

    volatile 关键字的练习

--- cpp_demo/myexercise/unuseless.cpp
+++ cpp_demo/myexercise/unuseless.cpp
@@ -25,10 +25,33 @@ private:
     int _month;
     int _day;
 };
+
+void demo9(){
+    //* volatile：系统总是重新从它所在的内存读取数据
+    //整型常量 加const 后 一些基本常量 会被编译器进行优化 存到寄存器里，后续直接使用寄存器里存的值
+    const volatile int a = 100;
+    const int c = 200;//整型常量
+
+    int *p = const_cast<int *>(&a); //常量引用被转为 非常量指针，仍指向原来的对象
+    int pp = const_cast<int&>(c); //去const的引用，进行了赋值操作，构造了新对象
+    int &ppp = const_cast<int &>(a);//去const 转换成引用，仍指向原来的对象
+
+
+    *p = 150;
+    pp = 250;
+    ppp = 300;
+    // 有 volatile 声明 a = 150； 无 volatile 声明 a = 100；
+    // 因为编译器优化 const a 的值存到了寄存器里，再输出时，默认取的寄存器的值 不是内存的值
+    cout<<"a = "<<a<<endl;
+    //下面输出的是指针内容，所以值是改变后的值，不是从寄存器拿的值
+    cout<<"*p = "<<*p<<endl;
+    cout<<"pp = "<<pp<<endl;
+    cout<<"ppp = "<<ppp<<endl;
+
+}
+
 int main()
 {
-    Date d1(2018,1,1);
-    //以下两种方法等价
-    Date d2(d1);
-    Date d3=d1;
+    demo9();
 }
+
