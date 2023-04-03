35d479a45d4551bbdd772c41c3242aeb87a24850
许营博 <gw00280825@tech.gwm.cn>
2023-04-03 16:51:39 +0800

    完善 tuple 练习 开始mem_fn练习

--- cpp_demo/Std_Function/std_tuple.cpp
+++ cpp_demo/Std_Function/std_tuple.cpp
@@ -28,6 +28,37 @@ std::tuple<double, char, std::string> get_student(int id)
     throw std::invalid_argument("id");
 }
 
+//* 递归展开 输出tuple 元素
+template<typename Tuple , size_t N>
+struct tuple_show
+{
+    static void show(const Tuple &t)
+    {
+        //输出最后一位元素的内容，然后递归调用函数获取下一位的内容
+        std::cout<< ", " << std::get<N - 1>(t);//模板类型是编译阶段定义的，因此这里的N-1不算做变量，可以用在std::get 里
+        tuple_show<Tuple, N - 1>::show(t);       
+    }
+};
+
+//* 偏特性，可以理解为递归的终止
+template<typename Tuple>
+struct tuple_show <Tuple,1>
+{
+    static void show(const Tuple &t)
+    {
+        //输出最后一位元素的内容，然后递归调用函数获取下一位的内容
+        std::cout<< ", " << std::get<0>(t);     
+    }
+};
+
+//调用函数
+template<typename... Args>
+void show_tuple(const std::tuple<Args...>& t){
+    std::cout<<"[";
+    //sizeof...(Args) : tuple 的类型数量
+    tuple_show<decltype(t), sizeof...(Args)>::show(t);
+    std::cout<<"]"<<std::endl;
+}
 
  
 int main()
@@ -76,5 +107,43 @@ int main()
               << "GPA: " << gpa3 << ", "
               << "grade: " << grade3 << ", "
               << "name: " << name3 << '\n';
+    //5.获取元组元素个数
+    std::tuple<char,int,long,std::string> tuple4('A',4,4.0,"F");
+    //tuple类型 写着比较长，用decltype 进行推断是一种不错的选择
+    int t4_count = std::tuple_size<decltype(tuple4)>::value;
+    std::cout<<"元组个数为 "<<t4_count<<std::endl;
+    //6、访问元组钟的元素
+    std::tuple<char,int,long,std::string> tuple5('A',4,4.0,"F");
+    std::cout<<"元组 第一个 元素 为 "<<std::get<0>(tuple5)<<std::endl;
+    std::cout<<"元组 int 元素 为 "<<std::get<int>(tuple5)<<std::endl;
+    //7、获取元组中元素的类型
+    std::tuple<char,int,long,std::string> tuple6('A',4,4.0,"F");
+    //获取指定位置类型 并声明变量
+    std::tuple_element<3,decltype(tuple6)>::type val_6;
+    //获取指定位置数据元素 并给声明的变量赋值
+    val_6 = std::get<3>(tuple6);
+    std::cout<<"val_6 为 "<<val_6<<std::endl;
+
+    //使用std::tie 解包
+    std::tuple<char,int,long,std::string> tuple7('A',4,4.0,"F");
+    // 定义变量，保存解包结果
+    char tuple_0    = '0';
+    int tuple_1        = 0;
+    long tuple_2    = 0;
+    std::string tuple_3("");
+    std::tie(tuple_0,std::ignore,tuple_2,tuple_3) = tuple7;
+    //C++17 结构化绑定,接收到的tuple对象内容，自动绑定到对应变量上（按顺序）
+    auto [t1,t2,t3,t4] = tuple7;
+    std::cout<<"tuple_0 = "<<tuple_0<<" t1 = "<<t1<<std::endl;
+    //元组拼接
+    std::tuple<char, int, double> first('A', 1, 2.2f);
+    // 组合到一起, 使用auto, 自动推导
+    auto second = std::tuple_cat(first, std::make_tuple('B', std::string("-=+")));
+    // 组合到一起，可以知道每一个元素的数据类型时什么 与 auto推导效果一样
+    std::tuple<char, int, double, char, std::string> tuple8 = std::tuple_cat(first, std::make_tuple('B', std::string("-=+")));
+    show_tuple(tuple8);
+
+    
+
 
 }
