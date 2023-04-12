55ae5e7cf0c780121db46dc9d11b323083ae9cc6
许营博 <gw00280825@tech.gwm.cn>
2023-04-12 18:05:12 +0800

    map 练习

---
+++ cpp_demo/myexercise/computColor.cpp
@@ -0,0 +1,626 @@
+/**********************************************************************************************************************
+ *  COPYRIGHT
+ *  -------------------------------------------------------------------------------------------------------------------
+ *  Copyright (c) 2022 by Gwm. All rights reserved.
+ *********************************************************************************************************************/
+
+#ifndef _ALCMCLRSETHMISRV_GS2S_DATATYPES_HPP_
+#define _ALCMCLRSETHMISRV_GS2S_DATATYPES_HPP_
+
+#include <cstdint>
+
+namespace GWM{
+namespace Vehicle{
+namespace Body{
+namespace ALCM{
+namespace GS2S{
+namespace ALCMClrSetHMISrv_GS2S{
+namespace ALCMClrSetHMISrv_GS2S_Gen{
+
+
+enum class ClrSet_Enum : std::uint8_t {
+    Colour_0 = 0,
+    Colour_1 = 1,
+    Colour_2 = 2,
+    Colour_3 = 3,
+    Colour_4 = 4,
+    Colour_5 = 5,
+    Colour_6 = 6,
+    Colour_7 = 7,
+    Colour_8 = 8,
+    Colour_9 = 9,
+    Colour_10 = 10,
+    Colour_11 = 11,
+    Colour_12 = 12,
+    Colour_13 = 13,
+    Colour_14 = 14,
+    Colour_15 = 15,
+    Colour_16 = 16,
+    Colour_17 = 17,
+    Colour_18 = 18,
+    Colour_19 = 19,
+    Colour_20 = 20,
+    Colour_21 = 21,
+    Colour_22 = 22,
+    Colour_23 = 23,
+    Colour_24 = 24,
+    Colour_25 = 25,
+    Colour_26 = 26,
+    Colour_27 = 27,
+    Colour_28 = 28,
+    Colour_29 = 29,
+    Colour_30 = 30,
+    Colour_31 = 31,
+    Colour_32 = 32,
+    Colour_33 = 33,
+    Colour_34 = 34,
+    Colour_35 = 35,
+    Colour_36 = 36,
+    Colour_37 = 37,
+    Colour_38 = 38,
+    Colour_39 = 39,
+    Colour_40 = 40,
+    Colour_41 = 41,
+    Colour_42 = 42,
+    Colour_43 = 43,
+    Colour_44 = 44,
+    Colour_45 = 45,
+    Colour_46 = 46,
+    Colour_47 = 47,
+    Colour_48 = 48,
+    Colour_49 = 49,
+    Colour_50 = 50,
+    Colour_51 = 51,
+    Colour_52 = 52,
+    Colour_53 = 53,
+    Colour_54 = 54,
+    Colour_55 = 55,
+    Colour_56 = 56,
+    Colour_57 = 57,
+    Colour_58 = 58,
+    Colour_59 = 59,
+    Colour_60 = 60,
+    Colour_61 = 61,
+    Colour_62 = 62,
+    Colour_63 = 63,
+    NoAction = 64,
+    Reserved1 = 65,
+    Reserved2 = 66,
+    Reserved3 = 67,
+    Reserved4 = 68,
+    Reserved5 = 69,
+    Reserved6 = 70,
+    Reserved7 = 71,
+    Reserved8 = 72,
+    Reserved9 = 73,
+    Reserved10 = 74,
+    Reserved11 = 75,
+    Reserved12 = 76,
+    Reserved13 = 77,
+    Reserved14 = 78,
+    Reserved15 = 79,
+    Reserved16 = 80,
+    Reserved17 = 81,
+    Reserved18 = 82,
+    Reserved19 = 83,
+    Reserved20 = 84,
+    Reserved21 = 85,
+    Reserved22 = 86,
+    Reserved23 = 87,
+    Reserved24 = 88,
+    Reserved25 = 89,
+    Reserved26 = 90,
+    Reserved27 = 91,
+    Reserved28 = 92,
+    Reserved29 = 93,
+    Reserved30 = 94,
+    Reserved31 = 95,
+    Reserved32 = 96,
+    Reserved33 = 97,
+    Reserved34 = 98,
+    Reserved35 = 99,
+    Reserved36 = 100,
+    Reserved37 = 101,
+    Reserved38 = 102,
+    Reserved39 = 103,
+    Reserved40 = 104,
+    Reserved41 = 105,
+    Reserved42 = 106,
+    Reserved43 = 107,
+    Reserved44 = 108,
+    Reserved45 = 109,
+    Reserved46 = 110,
+    Reserved47 = 111,
+    Reserved48 = 112,
+    Reserved49 = 113,
+    Reserved50 = 114,
+    Reserved51 = 115,
+    Reserved52 = 116,
+    Reserved53 = 117,
+    Reserved54 = 118,
+    Reserved55 = 119,
+    Reserved56 = 120,
+    Reserved57 = 121,
+    Reserved58 = 122,
+    Reserved59 = 123,
+    Reserved60 = 124,
+    Reserved61 = 125,
+    Reserved62 = 126,
+    Reserved63 = 127
+};
+
+enum class AtmLi_ColorSet_64_Enum : std::uint8_t {
+    Colour_0 = 0,
+    Colour_1 = 1,
+    Colour_2 = 2,
+    Colour_3 = 3,
+    Colour_4 = 4,
+    Colour_5 = 5,
+    Colour_6 = 6,
+    Colour_7 = 7,
+    Colour_8 = 8,
+    Colour_9 = 9,
+    Colour_10 = 10,
+    Colour_11 = 11,
+    Colour_12 = 12,
+    Colour_13 = 13,
+    Colour_14 = 14,
+    Colour_15 = 15,
+    Colour_16 = 16,
+    Colour_17 = 17,
+    Colour_18 = 18,
+    Colour_19 = 19,
+    Colour_20 = 20,
+    Colour_21 = 21,
+    Colour_22 = 22,
+    Colour_23 = 23,
+    Colour_24 = 24,
+    Colour_25 = 25,
+    Colour_26 = 26,
+    Colour_27 = 27,
+    Colour_28 = 28,
+    Colour_29 = 29,
+    Colour_30 = 30,
+    Colour_31 = 31,
+    Colour_32 = 32,
+    Colour_33 = 33,
+    Colour_34 = 34,
+    Colour_35 = 35,
+    Colour_36 = 36,
+    Colour_37 = 37,
+    Colour_38 = 38,
+    Colour_39 = 39,
+    Colour_40 = 40,
+    Colour_41 = 41,
+    Colour_42 = 42,
+    Colour_43 = 43,
+    Colour_44 = 44,
+    Colour_45 = 45,
+    Colour_46 = 46,
+    Colour_47 = 47,
+    Colour_48 = 48,
+    Colour_49 = 49,
+    Colour_50 = 50,
+    Colour_51 = 51,
+    Colour_52 = 52,
+    Colour_53 = 53,
+    Colour_54 = 54,
+    Colour_55 = 55,
+    Colour_56 = 56,
+    Colour_57 = 57,
+    Colour_58 = 58,
+    Colour_59 = 59,
+    Colour_60 = 60,
+    Colour_61 = 61,
+    Colour_62 = 62,
+    Colour_63 = 63
+};
+
+
+}//ALCMClrSetHMISrv_GS2S_Gen
+}//ALCMClrSetHMISrv_GS2S
+}//GS2S
+}//ALCM
+}//Body
+}//Vehicle
+}//GWM
+
+
+#endif /* _ALCMCLRSETHMISRV_GS2S_DATATYPES_HPP_ */
+/**********************************************************************************************************************
+ *  COPYRIGHT
+ *  -------------------------------------------------------------------------------------------------------------------
+ *  Copyright (c) 2022 by Gwm. All rights reserved.
+ *********************************************************************************************************************/
+
+#ifndef _ALCMCTRLSRV_DATATYPES_HPP_
+#define _ALCMCTRLSRV_DATATYPES_HPP_
+
+#include <cstdint>
+
+namespace GWM{
+namespace Vehicle{
+namespace Body{
+namespace ALCM{
+namespace ALCMCtrlSrv_Gen{
+
+
+using HardwireOutput_Command_Integer = std::uint16_t;
+
+using ColorCtrl_Command_Integer = std::uint8_t;
+
+enum class ModeSelectionCtrl_Command_Enum : std::uint8_t {
+    Static_Mode = 0,
+    Breathing_Mode = 1,
+    Flowing_Mode = 2,
+    Music_Mode = 3,
+    Dynamic_Mode = 4,
+    Random_Mode = 5,
+    Welcome_Mode = 6,
+    Flash_Frequency_Mode = 7
+};
+
+enum class LightLvlCtrl_Command_Enum : std::uint8_t {
+    OFF = 0,
+    per10 = 1,
+    per20 = 2,
+    per30 = 3,
+    per40 = 4,
+    per50 = 5,
+    per60 = 6,
+    per70 = 7,
+    per80 = 8,
+    per90 = 9,
+    per100 = 10,
+    Reserved1 = 11,
+    Reserved2 = 12,
+    Reserved3 = 13,
+    Reserved4 = 14,
+    Reserved5 = 15
+};
+
+enum class ModeChangeCtrl_Command_Enum : std::uint8_t {
+    Immediately = 0,
+    NON_Immediately = 1
+};
+
+using FadeTimeCtrl_Command_Integer = std::uint8_t;
+
+enum class SyllablePrmCtrl_Command_Enum : std::uint8_t {
+    No_Action = 0,
+    Valid1 = 1,
+    Valid2 = 2,
+    Valid3 = 3,
+    Valid4 = 4,
+    Valid5 = 5,
+    Valid6 = 6,
+    Valid7 = 7
+};
+
+enum class NodeSwtCtrl_Command_Enum : std::uint8_t {
+    OFF = 0,
+    ON = 1
+};
+
+using SOA_ReturnCode_Integer = std::uint8_t;
+
+using SOA_Reserved_Integer = std::uint8_t;
+
+enum class SOA_Protocol_Type_Enum : std::uint8_t {
+    Protocol_General = 0,
+    Protocol_SOMEIP = 1,
+    Protocol_DDS = 2,
+    Protocol_Custom = 128
+};
+
+enum class ActvNodeCtrl_Command_Enum : std::uint8_t {
+    Disable = 0,
+    Enable = 1
+};
+
+enum class SOA_ClientID_Emum : std::uint8_t {
+    LocalSwitch1 = 0,
+    LocalSwitch2 = 1,
+    LocalSwitch3 = 2,
+    HUT = 3,
+    RKE = 4,
+    PKE = 5,
+    BLE = 6,
+    Gesture = 7,
+    Voice = 8,
+    Induction = 9,
+    ADAS = 10,
+    AVP = 11,
+    IDC_L2 = 12,
+    Remote = 47,
+    Environment = 48,
+    Dark = 49,
+    Temp = 50,
+    Sunny = 51,
+    RLS = 52,
+    VehStateChg = 64,
+    DoorAction = 65,
+    Speed = 66,
+    Park = 67,
+    UserDefClnt1 = 240,
+    UserDefClnt2 = 241,
+    UserDefClnt3 = 242,
+    UserDefClnt4 = 243,
+    UserDefClnt5 = 244,
+    UserDefClnt6 = 245,
+    UserDefClnt7 = 246
+};
+
+struct ActvNodeCtrl_Struct {
+    using _Active_Node_1_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_1_type Active_Node_1;
+
+    using _Active_Node_2_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_2_type Active_Node_2;
+
+    using _Active_Node_3_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_3_type Active_Node_3;
+
+    using _Active_Node_4_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_4_type Active_Node_4;
+
+    using _Active_Node_5_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_5_type Active_Node_5;
+
+    using _Active_Node_6_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_6_type Active_Node_6;
+
+    using _Active_Node_7_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_7_type Active_Node_7;
+
+    using _Active_Node_8_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_8_type Active_Node_8;
+
+    using _Active_Node_9_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_9_type Active_Node_9;
+
+    using _Active_Node_10_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_10_type Active_Node_10;
+
+    using _Active_Node_11_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_11_type Active_Node_11;
+
+    using _Active_Node_12_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_12_type Active_Node_12;
+
+    using _Active_Node_13_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_13_type Active_Node_13;
+
+    using _Active_Node_14_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_14_type Active_Node_14;
+
+    using _Active_Node_15_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_15_type Active_Node_15;
+
+    using _Active_Node_16_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_16_type Active_Node_16;
+
+    using _Active_Node_17_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_17_type Active_Node_17;
+
+    using _Active_Node_18_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_18_type Active_Node_18;
+
+    using _Active_Node_19_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_19_type Active_Node_19;
+
+    using _Active_Node_20_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_20_type Active_Node_20;
+
+    using _Active_Node_21_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_21_type Active_Node_21;
+
+    using _Active_Node_22_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_22_type Active_Node_22;
+
+    using _Active_Node_23_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_23_type Active_Node_23;
+
+    using _Active_Node_24_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_24_type Active_Node_24;
+
+    using _Active_Node_1_NF_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_1_NF_type Active_Node_1_NF;
+
+    using _Active_Node_2_NF_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_2_NF_type Active_Node_2_NF;
+
+    using _Active_Node_3_NF_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_3_NF_type Active_Node_3_NF;
+
+    using _Active_Node_4_NF_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_4_NF_type Active_Node_4_NF;
+
+    using _Active_Node_5_NF_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_5_NF_type Active_Node_5_NF;
+
+    using _Active_Node_6_NF_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_6_NF_type Active_Node_6_NF;
+
+    using _Active_Node_7_NF_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_7_NF_type Active_Node_7_NF;
+
+    using _Active_Node_8_NF_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_8_NF_type Active_Node_8_NF;
+
+    using _Active_Node_9_NF_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_9_NF_type Active_Node_9_NF;
+
+    using _Active_Node_10_NF_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_10_NF_type Active_Node_10_NF;
+
+    using _Active_Node_11_NF_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_11_NF_type Active_Node_11_NF;
+
+    using _Active_Node_12_NF_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_12_NF_type Active_Node_12_NF;
+
+    using _Active_Node_13_NF_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_13_NF_type Active_Node_13_NF;
+
+    using _Active_Node_14_NF_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_14_NF_type Active_Node_14_NF;
+
+    using _Active_Node_15_NF_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_15_NF_type Active_Node_15_NF;
+
+    using _Active_Node_16_NF_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_16_NF_type Active_Node_16_NF;
+
+    using _Active_Node_17_NF_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_17_NF_type Active_Node_17_NF;
+
+    using _Active_Node_18_NF_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_18_NF_type Active_Node_18_NF;
+
+    using _Active_Node_19_NF_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_19_NF_type Active_Node_19_NF;
+
+    using _Active_Node_20_NF_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_20_NF_type Active_Node_20_NF;
+
+    using _Active_Node_21_NF_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_21_NF_type Active_Node_21_NF;
+
+    using _Active_Node_22_NF_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_22_NF_type Active_Node_22_NF;
+
+    using _Active_Node_23_NF_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_23_NF_type Active_Node_23_NF;
+
+    using _Active_Node_24_NF_type = ActvNodeCtrl_Command_Enum;
+    _Active_Node_24_NF_type Active_Node_24_NF;
+
+};
+
+struct SOA_Response_Struct {
+    using _ProtocolType_type = SOA_Protocol_Type_Enum;
+    _ProtocolType_type ProtocolType;
+
+    using _Reserved0_type = SOA_Reserved_Integer;
+    _Reserved0_type Reserved0;
+
+    using _Reserved1_type = SOA_Reserved_Integer;
+    _Reserved1_type Reserved1;
+
+    using _ReturnCode_type = SOA_ReturnCode_Integer;
+    _ReturnCode_type ReturnCode;
+
+};
+
+struct NodeSwtCtrl_Struct {
+    using _NodeSwtCtrl_type = NodeSwtCtrl_Command_Enum;
+    _NodeSwtCtrl_type NodeSwtCtrl;
+
+    using _NodeSwtCtrl_NF_type = NodeSwtCtrl_Command_Enum;
+    _NodeSwtCtrl_NF_type NodeSwtCtrl_NF;
+
+};
+
+struct SyllablePrmCtrl_Struct {
+    using _SyllablePrmCtrl_type = SyllablePrmCtrl_Command_Enum;
+    _SyllablePrmCtrl_type SyllablePrmCtrl;
+
+    using _SyllablePrmCtrl_NF_type = SyllablePrmCtrl_Command_Enum;
+    _SyllablePrmCtrl_NF_type SyllablePrmCtrl_NF;
+
+};
+
+struct FadeTimeCtrl_Struct {
+    using _FadeTimeCtrl_type = FadeTimeCtrl_Command_Integer;
+    _FadeTimeCtrl_type FadeTimeCtrl;
+
+    using _FadeTimeCtrl_NF_type = FadeTimeCtrl_Command_Integer;
+    _FadeTimeCtrl_NF_type FadeTimeCtrl_NF;
+
+};
+
+struct ModeChangeCtrl_Struct {
+    using _ModeChangeCtrl_type = ModeChangeCtrl_Command_Enum;
+    _ModeChangeCtrl_type ModeChangeCtrl;
+
+    using _ModeChangeCtrl_NF_type = ModeChangeCtrl_Command_Enum;
+    _ModeChangeCtrl_NF_type ModeChangeCtrl_NF;
+
+};
+
+struct LightLvlCtrl_Struct {
+    using _LightLvlCtrl_type = LightLvlCtrl_Command_Enum;
+    _LightLvlCtrl_type LightLvlCtrl;
+
+    using _LightLvlCtrl_NF_type = LightLvlCtrl_Command_Enum;
+    _LightLvlCtrl_NF_type LightLvlCtrl_NF;
+
+};
+
+struct ModeSelectionCtrl_Struct {
+    using _ModeSelectionCtrl_type = ModeSelectionCtrl_Command_Enum;
+    _ModeSelectionCtrl_type ModeSelectionCtrl;
+
+    using _ModeSelectionCtrl_NF_type = ModeSelectionCtrl_Command_Enum;
+    _ModeSelectionCtrl_NF_type ModeSelectionCtrl_NF;
+
+};
+
+struct ColorCtrl_Struct {
+    using _RedRatCtrl_type = ColorCtrl_Command_Integer;
+    _RedRatCtrl_type RedRatCtrl;
+
+    using _GreenRatCtrl_type = ColorCtrl_Command_Integer;
+    _GreenRatCtrl_type GreenRatCtrl;
+
+    using _BlueRatCtrl_type = ColorCtrl_Command_Integer;
+    _BlueRatCtrl_type BlueRatCtrl;
+
+};
+
+struct HardwireOutput_Struct {
+    using _HardwireGreenOutput_type = HardwireOutput_Command_Integer;
+    _HardwireGreenOutput_type HardwireGreenOutput;
+
+    using _HardwireBlueOutput_type = HardwireOutput_Command_Integer;
+    _HardwireBlueOutput_type HardwireBlueOutput;
+
+    using _HardwireRedOutput_type = HardwireOutput_Command_Integer;
+    _HardwireRedOutput_type HardwireRedOutput;
+
+};
+
+
+}//ALCMCtrlSrv_Gen
+}//ALCM
+}//Body
+}//Vehicle
+}//GWM
+
+
+#endif /* _ALCMCTRLSRV_DATATYPES_HPP_ */
+#include <map>
+#include <iostream>
+using AtmLi_ColorSet_64_Enum = GWM::Vehicle::Body::ALCM::GS2S::ALCMClrSetHMISrv_GS2S::ALCMClrSetHMISrv_GS2S_Gen::AtmLi_ColorSet_64_Enum;
+using ColorCtrl_Struct = GWM::Vehicle::Body::ALCM::ALCMCtrlSrv_Gen::ColorCtrl_Struct;
+using R_ColorType = GWM::Vehicle::Body::ALCM::ALCMCtrlSrv_Gen::ColorCtrl_Struct::_RedRatCtrl_type;
+using G_ColorType = GWM::Vehicle::Body::ALCM::ALCMCtrlSrv_Gen::ColorCtrl_Struct::_GreenRatCtrl_type;
+using B_ColorType = GWM::Vehicle::Body::ALCM::ALCMCtrlSrv_Gen::ColorCtrl_Struct::_BlueRatCtrl_type;
+
+std::map <AtmLi_ColorSet_64_Enum,ColorCtrl_Struct> colormap{
+    {AtmLi_ColorSet_64_Enum::Colour_0,ColorCtrl_Struct{R_ColorType(static_cast<uint8_t>(5*7+10)),G_ColorType(static_cast<uint8_t>(113)),B_ColorType(static_cast<uint8_t>(2))}}
+};
+
+
+ColorCtrl_Struct return_rgbcolor(AtmLi_ColorSet_64_Enum color_id){
+    ColorCtrl_Struct a{0,0,0};
+    return a;
+}
+
+int main(){
+
+    std::cout<<static_cast<int>(colormap[AtmLi_ColorSet_64_Enum::Colour_0].RedRatCtrl);
+
+    return 0;
+}
