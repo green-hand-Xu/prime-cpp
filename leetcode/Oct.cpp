a10e10dc9868dbc6d972fee9b0942edbe0b78ad5
green-hand-Xu <837085581@qq.com>
2023-10-23 01:22:44 +0800

    11

--- leetcode/Oct.cpp
+++ leetcode/Oct.cpp
@@ -88,7 +88,135 @@ void TestSolutionTwo(){
     cout<<SolutionTwo.canPlaceFlowers(flower,n)<<endl;
 }
 
+/** 反转字符串中的元音字母
+        给你一个字符串 s ，仅反转字符串中的所有元音字母，并返回结果字符串。元音字母包括 'a'、'e'、'i'、'o'、'u'，且可能以大小写两种形式出现不止一次。
+*/
+class SolutionThree {
+public:
+    string YY{"aeiouAEIOU"};
+    string reverseVowels(string s) {
+        
+        std::vector<int> YYIndex;
+        //* 用一个数组 记录字符中所有的元音字符的位置
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (copare(s[i]))
+            {
+                YYIndex.push_back(i);
+            }
+        }
+        if(YYIndex.empty()) return s;
+
+        int head = 0;
+        int tail = YYIndex.size();
+        int mid = tail/2;
+        
+        for (auto i = head; i < mid; i++)
+        {
+            if (head<=tail)
+            {   
+                auto temp = s[YYIndex[i]];
+                s[YYIndex[i]] = s[YYIndex[tail-1]];
+                s[YYIndex[tail-1]] =temp;
+            } 
+            tail--;
+        }
+        return s;
+    }
+
+    bool copare(char s){
+        if(YY.find(s)!=string::npos) return true;
+        return false;
+    }
+}SolutionThree;
+
+void TestThree(){
+    string str{"aeiou"};
+    cout<<SolutionThree.reverseVowels(str)<<endl;
+}
+
+/** 反转字符串中的单词
+        给你一个字符串 s ，请你反转字符串中 单词 的顺序。
+        单词 是由非空格字符组成的字符串。s 中使用至少一个空格将字符串中的 单词 分隔开。
+        返回 单词 顺序颠倒且 单词 之间用单个空格连接的结果字符串。
+        注意：输入字符串 s中可能会存在前导空格、尾随空格或者单词间的多个空格。返回的结果字符串中，单词间应当仅用单个空格分隔，且不包含任何额外的空格。
+*/
+class SolutionFour {
+public:
+    vector<string> words;//把读出来的单词进行存储 然后在倒置顺序
+    string reverseWords(string s) {
+        s = ' '+s+' ';
+        sortWords(s);
+        string str{""};
+        int index = words.size()-1;
+        while (index > 0)
+        {
+            str = str + words[index]+ ' ';
+            index--;
+        }
+        str = str+words[0];
+        return str;
+    }
+
+    string sortWords(string s){
+        int re = 0;//左下标
+        int ri = 0;//右下标
+        for(int i = 0; i<s.size();i++){
+            if (s[i] == ' ' || i == s.size()-1)
+            {
+                if (re < ri)
+                {
+                    words.push_back(s.substr(re+1,ri-re));
+                }
+                re = i;
+                continue;
+            }
+            ri = i;
+        }
+        return s;
+    }
+//* 力扣官方题解
+    string reverseWords_leetcode(string s) {
+        // 反转整个字符串
+        reverse(s.begin(), s.end());
+
+        int n = s.size();
+        int idx = 0;
+        for (int start = 0; start < n; ++start) {
+            if (s[start] != ' ') {
+                // 填一个空白字符然后将idx移动到下一个单词的开头位置
+                if (idx != 0) s[idx++] = ' ';
+
+                // 循环遍历至单词的末尾
+                int end = start;
+                while (end < n && s[end] != ' ') s[idx++] = s[end++];
+
+                // 反转整个单词
+                reverse(s.begin() + idx - (end - start), s.begin() + idx);
+
+                // 更新start，去找下一个单词
+                start = end;
+            }
+        }
+        s.erase(s.begin() + idx, s.end());
+        return s;
+    }
+
+}SolutionFour;
+
+void TestFour(){
+    string s = "the sky is blue";
+    auto str = SolutionFour.reverseWords(s);
+    for (auto word : SolutionFour.words)
+    {
+        cout<<word;
+    }
+    cout<<endl;
+    cout << str<<endl;
+    
+}
+
 int main(){
-    TestSolutionTwo();
+    TestFour();
     return 0;
 }
