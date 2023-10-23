d8efa979ca6bdebc01319e2efccf99e65d227a72
green-hand-Xu <837085581@qq.com>
2023-10-24 01:25:37 +0800

    除自身以外数组的乘积

--- leetcode/Oct.cpp
+++ leetcode/Oct.cpp
@@ -178,7 +178,7 @@ public:
     }
 //* 力扣官方题解
 /*
-方法二：自行编写对应的函数
+方法二：自.行编写对应的函数
 思路和算法
 我们也可以不使用语言中的 API，而是自己编写对应的函数。在不同语言中，这些函数实现是不一样的，主要的差别是有些语言的字符串不可变（如 Java 和 Python)，有些语言的字符串可变（如 C++)。
 对于字符串不可变的语言，首先得把字符串转化成其他可变的数据结构，同时还需要在转化的过程中去除空格。
@@ -271,7 +271,97 @@ void TestFour(){
     
 }
 
+/** 除自身以外数组的乘积
+给你一个整数数组 nums，返回 数组 answer ，其中 answer[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积 。
+题目数据 保证 数组 nums之中任意元素的全部前缀元素和后缀的乘积都在  32 位 整数范围内。
+** 请不要使用除法，且在 O(n) 时间复杂度内完成此题。
+*/
+/*
+*方法一：左右乘积列表
+*思路:
+    我们不必将所有数字的乘积除以给定索引处的数字得到相应的答案，而是利用索引左侧所有数字的乘积和右侧所有数字的乘积（即前缀与后缀）相乘得到答案。
+    对于给定索引 iii，我们将使用它左边所有数字的乘积乘以右边所有数字的乘积。下面让我们更加具体的描述这个算法。
+
+*算法:
+    初始化两个空数组 L 和 R。对于给定索引 i，L[i] 代表的是 i 左侧所有数字的乘积，R[i] 代表的是 i 右侧所有数字的乘积。
+    我们需要用两个循环来填充 L 和 R 数组的值。对于数组 L，L[0] 应该是 1，
+    因为第一个元素的左边没有元素。对于其他元素：L[i] = L[i-1] * nums[i-1]。
+    同理，对于数组 R，R[length-1] 应为 1。length 指的是输入数组的大小。其他元素：R[i] = R[i+1] * nums[i+1]。
+    当 R 和 L 数组填充完成，我们只需要在输入数组上迭代，且索引 i 处的值为：L[i] * R[i]。
+*/
+
+class SolutionFive {
+public:
+    vector<int> productExceptSelf_One(vector<int>& nums) {
+        int length = nums.size();
+
+        // L 和 R 分别表示左右两侧的乘积列表
+        vector<int> L(length, 0), R(length, 0);
+
+        vector<int> answer(length);
+
+        //往L数组赋值
+        //* 对于索引为 '0' 的元素，因为左侧没有元素，所以 L[0] = 1
+        L[0] = 1;
+        for (auto i = 1; i < length; i++)
+        {
+            L[i] = nums[i-1] * L[i-1];
+        }
+        //往R数组赋值
+        //*对于索引为 'length-1' 的元素，因为右侧没有元素，所以 R[length-1] = 1
+        R[length - 1] = 1;
+        for (auto i = length-2; i >= 0; i--)
+        {
+            R[i] = nums[i+1] * R[i+1];
+        }
+        
+        // 对于索引 i，除 nums[i] 之外其余各元素的乘积就是左侧所有元素的乘积乘以右侧所有元素的乘积
+        for (int i = 0; i < length; i++) {
+            answer[i] = L[i] * R[i];
+        }
+
+        return answer;
+    }
+/*
+*方法二：空间复杂度 O(1) 的方法
+*思路
+尽管上面的方法已经能够很好的解决这个问题，但是空间复杂度并不为常数。
+由于输出数组不算在空间复杂度内，那么我们可以将 L 或 R 数组用输出数组来计算。先把输出数组当作 L 数组来计算，然后再动态构造 R 数组得到结果。让我们来看看基于这个思想的算法。
+
+*算法
+初始化 answer 数组，对于给定索引 i，answer[i] 代表的是 i 左侧所有数字的乘积。
+构造方式与之前相同，只是我们试图节省空间，先把 answer 作为方法一的 L 数组。
+这种方法的唯一变化就是我们没有构造 R 数组。而是用一个遍历来跟踪右边元素的乘积。并更新数组 answer[i]=answer[i]∗Ranswer[i]=answer[i]*Ranswer[i]=answer[i]∗R。然后 RRR 更新为 R=R∗nums[i]R=R*nums[i]R=R∗nums[i]，其中变量 RRR 表示的就是索引右侧数字的乘积。
+*/
+    vector<int> productExceptSelf_Two(vector<int>& nums) {
+        int length = nums.size();
+        vector<int> answer(length);
+
+        // answer[i] 表示索引 i 左侧所有元素的乘积
+        // 因为索引为 '0' 的元素左侧没有元素， 所以 answer[0] = 1
+        answer[0] = 1;
+        for (int i = 1; i < length; i++) {
+            answer[i] = nums[i - 1] * answer[i - 1];
+        }
+
+        // R 为右侧所有元素的乘积
+        // 刚开始右边没有元素，所以 R = 1
+        int R = 1;
+        for (int i = length - 1; i >= 0; i--) {
+            // 对于索引 i，左边的乘积为 answer[i]，右边的乘积为 R
+            answer[i] = answer[i] * R;
+            // R 需要包含右边所有的乘积，所以计算下一个结果时需要将当前值乘到 R 上
+            R *= nums[i];
+        }
+        return answer;
+    }
+}SolutionFive;
+
+void TestFive(){
+
+}
+
 int main(){
-    TestFour();
+    TestFive();
     return 0;
 }
