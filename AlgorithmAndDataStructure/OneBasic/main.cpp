5ff19e3a265b4190e6f56214257e0b8ae88011ac
许营博 <gw00280825@tech.gwm.cn>
2023-10-25 21:01:44 +0800

    10.25

--- AlgorithmAndDataStructure/OneBasic/main.cpp
+++ AlgorithmAndDataStructure/OneBasic/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <functional>
 class Date{
   public:
     Date(){}
@@ -18,13 +18,55 @@ class Date{
     int year{0};
 };
 
+int FUN1(int a){
+  if (a == 0)
+  {
+    return 0;
+  }
+  if (a == 1)
+  {
+    return 1;
+  }
+  return FUN1(a-1)+FUN1(a-2);
+}
+
+int FUN(int a){
+  return FUN1(a);
+}
 
 
+
+void Test(){
+  for (int i = 0; i < 100; i++)
+  {
+    std::cout<<FUN(i)<<std::endl;
+  }
+  
+}
+
+struct lambda{
+  int operator ()(){
+    return ++x;
+  }
+
+  private:
+  int x=0;
+};
+
+std::function<int()>  meta_add(){
+  lambda add;
+  return add;
+}
+
+void TestTwo(){
+  auto fun = meta_add();
+
+  std::cout<<fun()<<std::endl;
+  std::cout<<fun()<<std::endl;
+  std::cout<<fun()<<std::endl;
+}
 
 int main(){
-  Date a;
-  Date b;
-  std::cout<<a.equals(&b)<<std::endl;
-
+  Test();
   return 0;
 }
