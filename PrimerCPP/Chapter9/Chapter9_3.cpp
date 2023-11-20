978dc1d82b150e59269372969fe5f69b38dfaa17
许营博 <gw00280825@tech.gwm.cn>
2023-11-20 17:54:17 +0800

    11.20

--- PrimerCPP/Chapter9/Chapter9_3.cpp
+++ PrimerCPP/Chapter9/Chapter9_3.cpp
@@ -1,8 +1,30 @@
+/**
+ * @file Chapter9_3.cpp
+ * @author XuYingBo (you@domain.com)
+ * @brief 顺序容器操作练习
+ * @version 0.1
+ * @date 2023-11-20
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
 #include <iostream>
 
+#include <list>
+#include <deque>
+#include <vector>
+#include <forward_list>
+#include <string>
+#include <array>
 
-
-
+using namespace std;
+// 默认初始化
+list<int> ilist; //双向链表
+deque<int> ideque; //双向队列
+vector<int> ivector; //单向向量
+forward_list<int> i_flist;//前向链表
+string str;// 字符容器
+array<int,20> iarray; // 数组
 
 int main(){
     
