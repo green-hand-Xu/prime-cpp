f427bb519a405c6c4cb1e2036f81860ead256420
green-hand-Xu <837085581@qq.com>
2023-11-20 23:30:54 +0800

    Primer课件附件代码

--- PrimerCPP/Chapter9/Chapter9_3.cpp
+++ PrimerCPP/Chapter9/Chapter9_3.cpp
@@ -18,16 +18,21 @@
 #include <array>
 
 using namespace std;
-// 默认初始化
-list<int> ilist; //双向链表
-deque<int> ideque; //双向队列
-vector<int> ivector; //单向向量
-forward_list<int> i_flist;//前向链表
-string str;// 字符容器
-array<int,20> iarray; // 数组
+//双向链表
+list<int> ilist(10,0);// 构造一个 包含10个0的list 
+//双向队列
+deque<int> ideque(10); // 构造包含10个int的deque 使用默认初始化 int 0
+//单向向量
+vector<int> ivector(ilist.begin(),ilist.end()); // 构造一个vector 容器，初始化为迭代器指定范围中的元素拷贝
+//前向链表
+forward_list<int> i_flist{1,2,3}; //构造一个forward_list 使用列表初始化
+// 字符容器
+string str;
+// 数组
+array<int,20> iarray; 
 
 int main(){
     
-
+    
     return 0;
 }
