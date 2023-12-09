1ba7eb0a6ac7ce68e0c9aa5acb06f083be4c50d6
许营博 <837085581@qq.com>
2023-12-09 17:50:11 +0800

    12.9

--- PrimerCPP/Chapter9/Chapter9_3.cpp
+++ PrimerCPP/Chapter9/Chapter9_3.cpp
@@ -492,13 +492,42 @@ void queueAdapter(){
 
 /**
  * @brief 练习9.52
- * 
+    使用stack处理括号化的表达式。当你看到一个左括号，将其记录下来。
+    当你在一个左括号之后看到一个右括号，从stack中pop对象，直至遇到左括号，将左括号也一起弹出栈。
+    然后将一个值（括号内的运算结果）push到栈中，表示一个括号化的（子）表达式已经处理完毕，被其运算结果所替代。
  */
 void parenthesized_expressions(){
+    string hello{"(hello) world"};
+    bool bSeen = false;
+    stack<char> stk;
+
+    for (const auto& s: hello)
+    {
+        if (s == '(') { 
+            bSeen = true; continue; 
+        }else if (s == ')') {
+            bSeen = false;
+        }
+        
+        if (bSeen) {
+            stk.push(s);// hello  入栈顺序
+        }  
+    }
+
+    string outstring{""};
+    while (!stk.empty())
+    {
+        outstring += stk.top();//olleh 出栈顺序
+        stk.pop();
+    }
+    cout<<outstring<<endl;
+    hello.replace(hello.find("(")+1, outstring.size(), outstring);
+
+    cout<<hello<<endl;
 
 }
 
 int main(){
-    queueAdapter();
+    parenthesized_expressions();
     return 0;
 }
