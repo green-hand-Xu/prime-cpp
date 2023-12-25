10e97cb5b93f5a1f0abd9bcef7fd99c57addb13c
许营博 <837085581@qq.com>
2023-12-25 18:02:57 +0800

    1

--- Designpatterns/DynamicReflection/DynReflection.cpp
+++ Designpatterns/DynamicReflection/DynReflection.cpp
@@ -8,6 +8,7 @@
  */
 #include <iostream>
 #include <string>
+#include <type_traits>
 
 // 定义宏__FUNC__ 表示
 #ifdef _WIN64
@@ -17,20 +18,72 @@
 #endif
 
 using namespace std;
+// 定义的动态获取的类类型信息
+enum class DataType{USB,PCI,HD,NOT};
+enum  DType{USB,PCI,HD,NOT};
 
-namespace DynReflection_Space {
-void globalfunc(int a,int b){
-    cout<<__FUNC__<<endl;
+//* 利用宏+字符串截取，输出T的类型信息
+template<typename T>
+std::string_view TypeInfo(){
+    string type = __FUNC__;
+    auto begin = type.find("T = ")+4;
+    auto end = type.find(";");
+    return std::string_view{type.data()+begin , end - begin};
 }
-} // DynReflection_Space
 
+//* 带模板的仿函数类 在c++ 17后可以用带模板的lambda 表达式实现类似功能
+template <int s, int e>
+struct static_for
+{
+    template<typename F>
+    void operator()(const F& func) const
+    {
+        if (s < e)
+        {
+            //注意：一定要转成常量
+            func(std::integral_constant<int,s>());
+            static_for<s + 1, e>()(func);
+        }
+    }
+};
 
+template <int n>
+struct static_for<n, n>
+{
+    template<typename F>
+    void operator()( const F & func) const
+    {
+       // std::cout << "noting" << std::endl;
+    }
+};
 
+constexpr int s = static_cast<int>(DataType::USB);
+constexpr int e = static_cast<int>(DataType::NOT);
 
+template<typename T>
+std::string_view Typeof(T t)
+{
+   int t1 = static_cast<int>(t);
+   std::string_view sv;
+       static_for<s, e>()([&](auto num) {
+        if (t1 == num) {
+             //重点要理解const和num.value
+             const DataType d = DataType{num.value};
+            sv = TypeInfo<d>();
+            }
+            });
 
+    return sv;
+}
 
-int main(){
-    DynReflection_Space::globalfunc(1,2);
+void getTypeTest()
+{
+    std::cout << Typeof(DataType::HD) << std::endl;
+    //std::cout << Typeof<DType>(DType::PCI) << std::endl;
+}
 
-    return 0;
+int main()
+{
+        getTypeTest();
+        return 0;
 }
