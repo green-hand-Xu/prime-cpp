e8a1c583334cbe26a71731f819bcea42271d0506
许营博 <837085581@qq.com>
2024-01-04 20:29:11 +0800

    加入解包位移代码

--- cpp_demo/myexercise/exercese/Project_Method_Testing/shift.cpp
+++ cpp_demo/myexercise/exercese/Project_Method_Testing/shift.cpp
@@ -65,3 +65,66 @@ static inline uint8_t pack_right_shift_u64(
 {
     return static_cast<uint8_t>(static_cast<uint8_t>(value >> shift) & mask);
 }
+static inline uint8_t unpack_left_shift_u8(
+    uint8_t value,
+    uint8_t shift,
+    uint8_t mask)
+{
+    return (uint8_t)((uint8_t)(value & mask) << shift);
+}
+
+static inline uint16_t unpack_left_shift_u16(
+    uint8_t value,
+    uint8_t shift,
+    uint8_t mask)
+{
+    return (uint16_t)((uint16_t)(value & mask) << shift);
+}
+
+static inline uint32_t unpack_left_shift_u32(
+    uint8_t value,
+    uint8_t shift,
+    uint8_t mask)
+{
+    return (uint32_t)((uint32_t)(value & mask) << shift);
+}
+
+static inline uint64_t unpack_left_shift_u64(
+    uint8_t value,
+    uint8_t shift,
+    uint8_t mask)
+{
+    return (uint64_t)((uint64_t)(value & mask) << shift);
+}
+
+static inline uint8_t unpack_right_shift_u8(
+    uint8_t value,
+    uint8_t shift,
+    uint8_t mask)
+{
+    return (uint8_t)((uint8_t)(value & mask) >> shift);
+}
+
+static inline uint16_t unpack_right_shift_u16(
+    uint8_t value,
+    uint8_t shift,
+    uint8_t mask)
+{
+    return (uint16_t)((uint16_t)(value & mask) >> shift);
+}
+
+static inline uint32_t unpack_right_shift_u32(
+    uint8_t value,
+    uint8_t shift,
+    uint8_t mask)
+{
+    return (uint32_t)((uint32_t)(value & mask) >> shift);
+}
+
+static inline uint64_t unpack_right_shift_u64(
+    uint8_t value,
+    uint8_t shift,
+    uint8_t mask)
+{
+    return (uint64_t)((uint64_t)(value & mask) >> shift);
+}
